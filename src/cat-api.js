ed998f4212e2657002c34975ec1e2b5d52de9942
Ivan <120274876+Gelsomin0@users.noreply.github.com>
2023-06-05 21:41:54 +0300

    Cat API done

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,5 +1,6 @@
 const API_KEY = 'live_4jedRlAWSRx3jFyMn5M3JGRQ8rFrFURgp3h7GLTPmakUBvLr0FdhVLk6nvXpBVyl';
 const BREEDS_URL = 'https://api.thecatapi.com/v1/breeds';
+const BASE_ID_URL = 'https://api.thecatapi.com/v1/images/search';
 const options = {
     headers: {
         'x-api-key': API_KEY,
@@ -14,5 +15,13 @@ export class CatApiService {
             .then((res) => {
                 return res.json();
             })
+    }
+
+    fetchCatByBreed(breedId) {
+        return fetch(`${BASE_ID_URL}?breed_ids=${breedId}`, options).then((res) => {
+            return res.json();
+        }).then((res) => {
+            return res[0];
+        })
     }
 }
--- src/index.js
+++ src/index.js
@@ -3,14 +3,23 @@ import { CatApiService } from "./cat-api"
 const catApiService = new CatApiService();
 const ell = {
     breedSelect: document.querySelector('.breed-select'),
+    catInfo: document.querySelector('.cat-info'),
+    loader: document.querySelector('.loader'),
+    error: document.querySelector('.error'),
 }
 
 getCatBreeds();
+ell.breedSelect.addEventListener('change', getCatInfo);
 
 
 function getCatBreeds() {
+    ell.loader.classList.add('js-visible');
+
     catApiService.fetchBreeds().then((res) => {
         setCatNamesList(res);
+    }).catch((err) => {
+        ell.loader.classList.remove('js-visible');
+        ell.error.classList.add('js-visible');
     })
 }
 
@@ -24,4 +33,40 @@ function setCatNamesList(list) {
     }).join('');
 
     ell.breedSelect.insertAdjacentHTML('beforeend', catNamesList);
+    ell.breedSelect.classList.add('js-visible');
+    ell.loader.classList.remove('js-visible');
+}
+
+function getCatInfo(e) {
+    ell.loader.classList.add('js-visible');
+    ell.catInfo.classList.remove('js-visible');
+    ell.error.classList.remove('js-visible');
+
+    catApiService.fetchCatByBreed(e.currentTarget.value).then((res) => {
+        const { breeds, url } = res;
+
+        const catInfo =  breeds.map(({ name, description, temperament }) => {
+            return `
+            <div>
+                <img class='cat-image' src='${url}'>
+            </div>
+            <div>
+                <h2>${name}</h2>
+                <p>${description}</p>
+                <p>
+                    <span class='evidance'>Temperament: </span> ${temperament}
+                </p>
+            </div>
+            `
+        }).join('');
+
+        ell.catInfo.innerHTML = catInfo;
+        ell.catInfo.classList.add('js-visible');
+        ell.loader.classList.remove('js-visible');
+    }).catch((err) => {
+        console.log('Opps! Some error!', err);
+
+        ell.loader.classList.remove('js-visible');
+        ell.error.classList.add('js-visible');
+    });
 }
